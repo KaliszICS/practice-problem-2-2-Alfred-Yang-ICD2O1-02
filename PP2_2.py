802403d491d173a9882e97419e69bb5070978699
Alfred-Yang-ICD2O1-02 <alfred.yang@student.tdsb.on.ca>
2024-10-15 18:30:43 +0000

    fwaibnafwio

--- PP2_2.py
+++ PP2_2.py
@@ -2,15 +2,43 @@
 
 def q1(): 
   #Write Assignment code here
+  num = int(input("Input an integer: "))
 
+  if num == 5:
+    print ("The number is Five")
+  else:
+    print ("The number is not Five")
 
 def q2(): 
   #Write Assignment code here
+  num2 = float(input("Input a number: "))
+  if num2 > 0:
+    print ("Positive")
+  else:
+    print ("Negative")
+
+def q3():
+  even = int(input("Input an integer: "))
+  if even % 2 == 0:
+    print ("Even")
+  else:
+    print("Odd")
+
+
+
+def q4():
+  hello = input('Type "Hello": ')
+  if hello == "Hello":
+    print ("The word is Hello")
+  else:
+    print ("The word is not Hello")
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
+# q1()
+# q2()
+# q3()
+# q4()
